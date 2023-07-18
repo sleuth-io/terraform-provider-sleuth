1a44f281b35ce97d49ee3a0f4e35ab36815355af
Jure Malovrh <jmalovrh@sleuth.io>
2023-07-18 13:38:01 +0200

    Add new Incident impact resource as example

--- examples/resources/sleuth_incident_impact_source/resource.tf
+++ examples/resources/sleuth_incident_impact_source/resource.tf
@@ -8,3 +8,16 @@ resource "sleuth_incident_impact_source" "pd" {
         remote_urgency = "ANY"
     }
 }
+
+resource "sleuth_incident_impact_source" "dd" {
+    project_slug = "project_slug"
+    name = "DataDog TF incident impact"
+    environment_name = "environment_name"
+    provider_name = "DATADOG"
+    datadog_input {
+        query = "@query=123" # use @ if you are using facets in DataDog
+        remote_priority_threshold = "ALL" # or P1 to P5
+        integration_slug = "optional_integration_slug"
+    }
+}
+
