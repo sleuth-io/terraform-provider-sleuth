c19ac9380c7b25e2f068a3490bbff741278cb6ad
Jure Malovrh <jmalovrh@sleuth.io>
2023-07-21 11:27:31 +0200

    Add TF5 muxing option
    Currently we want to support TF < v1. For that reason we need to still serve
    GRPC version 5. In ths PR we introduce new muxing that will translate new
    Framework providers into old SDK providers response.

--- main.go
+++ main.go
@@ -1,15 +1,5 @@
 package main
 
-import (
-	"context"
-	"flag"
-	"log"
-
-	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
-
-	"github.com/sleuth-io/terraform-provider-sleuth/internal/provider"
-)
-
 // Run "go generate" to format example terraform files and generate the docs for the registry/website
 
 // If you do not have terraform installed, you can remove the formatting command, but its suggested to
@@ -20,30 +10,57 @@ import (
 // can be customized.
 //go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs
 
-var (
-	// these will be set by the goreleaser configuration
-	// to appropriate values for the compiled binary
-	version string = "dev"
+import (
+	"context"
+	"flag"
+	"log"
 
-	// goreleaser can also pass the specific commit if you want
-	// commit  string = ""
+	"github.com/hashicorp/terraform-plugin-framework/providerserver"
+	"github.com/hashicorp/terraform-plugin-go/tfprotov5"
+	"github.com/hashicorp/terraform-plugin-go/tfprotov5/tf5server"
+	"github.com/hashicorp/terraform-plugin-mux/tf5muxserver"
+
+	sdkProvider "github.com/sleuth-io/terraform-provider-sleuth/internal/provider"
+	frameworkProvider "github.com/sleuth-io/terraform-provider-sleuth/internal/sleuth"
+)
+
+var (
+	// Version can be updated by goreleaser on release
+	version string = "dev"
 )
 
 func main() {
-	var debugMode bool
+	ctx := context.Background()
 
-	flag.BoolVar(&debugMode, "debug", false, "set to true to run the provider with support for debuggers like delve")
+	var debug bool
+
+	flag.BoolVar(&debug, "debug", false, "set to true to run the provider with support for debuggers like delve")
 	flag.Parse()
 
-	opts := &plugin.ServeOpts{ProviderFunc: provider.New(version)}
-
-	if debugMode {
-		err := plugin.Debug(context.Background(), "sleuth.io/core/sleuth", opts)
-		if err != nil {
-			log.Fatal(err.Error())
-		}
-		return
+	sdkGrpcProvider := sdkProvider.New(version)().GRPCProvider
+	providers := []func() tfprotov5.ProviderServer{
+		providerserver.NewProtocol5(frameworkProvider.New()),
+		sdkGrpcProvider,
 	}
 
-	plugin.Serve(opts)
+	muxServer, err := tf5muxserver.NewMuxServer(ctx, providers...)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var serveOpts []tf5server.ServeOpt
+
+	if debug {
+		serveOpts = append(serveOpts, tf5server.WithManagedDebug())
+	}
+	err = tf5server.Serve(
+		"sleuth.io/core/sleuth",
+		muxServer.ProviderServer,
+		serveOpts...,
+	)
+
+	if err != nil {
+		log.Fatal(err)
+	}
 }
