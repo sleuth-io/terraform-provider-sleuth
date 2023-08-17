acf666042cd4b34e26103277ed6df0948a8a8d82
Jure Malovrh <jmalovrh@sleuth.io>
2023-08-17 14:58:51 +0200

    Rewrite metric impact resource using Plugin SDK

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -49,8 +49,8 @@ func New(version string) func() *schema.Provider {
 			ResourcesMap: map[string]*schema.Resource{
 				//"sleuth_project":                resourceProject(),
 				//"sleuth_environment":          resourceEnvironment(),
-				"sleuth_error_impact_source":  resourceErrorImpactSource(),
-				"sleuth_metric_impact_source": resourceMetricImpactSource(),
+				"sleuth_error_impact_source": resourceErrorImpactSource(),
+				//"sleuth_metric_impact_source": resourceMetricImpactSource(),
 				//"sleuth_code_change_source":     resourceCodeChangeSource(),
 				"sleuth_incident_impact_source": resourceIncidentImpactSource(),
 			},
